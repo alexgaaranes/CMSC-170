42b3a6fe325578782cbe6547afc2be731bff061a
alexgaaranes <aaaranes@up.edu.ph>
2025-09-12 00:55:37 +0800

    revamped tictactoe

--- Tic-Tac-Toe/aranes_exer00-2.py
+++ Tic-Tac-Toe/aranes_exer00-2.py
@@ -1,145 +1,80 @@
 # Responsible Use of AI:
 # Extent and Purpose of AI Use:
-#   AI was used during the searching/reviewing of python syntax through google.
-#   Google implemented AI Overview via Gemini summarizing the content I was searching for.
+#
 # Responsible Use Justification:
-#   The AI was only used for reviewing python syntax especially the built-in methods for objects.
-#   Any code generation or direct prompting for answers was never done. Only for searching purposes.
-
+#
 
 # Name:     Alexander Gabriel A. Aranes
 # Section:  EF-4L
 
 # Tic-Tac-Toe
 
-# Static class for the game grid
-class GridMap:
-    turn = True    # pertains to 'X' as first turn
-    numOfTurns = 0
-    hasWinner = False
-    map = None
+# separate class for the board of the game
+class Board:
+    grid: list = None
 
-    def clear(): 
-        GridMap.map = [
+    def __init__(self):
+        self.grid = [
             ['','',''],
             ['','',''],
             ['','','']
         ]
-        GridMap.numOfTurns = 0
-        GridMap.turn = True
-        GridMap.hasWinner = False
-
     
-    # Decode the array element for showing
-    def decode(e: str):
-        if e == '':
-            print("   |", end="")
+    def show(self):
+        for row in self.grid:
+            for cell in row:
+                print(f'| {' ' if cell == '' else cell} ', end='')
+            print('|')
+    
+    def put(self, turn: str, pos: tuple) -> None:
+        if pos[0] > 2 or pos[0] < 0 or pos[1] > 2 or pos[1] < 0:
+            print("Invalid Position")
         else:
-            print(f" {e} |", end="")
-
-    # Formats to the stdout the map
-    def show():
-        print("    1 | 2 | 3 |")
+            if self.grid[pos[0]][pos[1]] != '': print("Occupied")
+            else: self.grid[pos[0]][pos[1]] = turn
+    
+    def winner(self) -> str:
         for i in range(3):
-            print(f"{i+1} |", end="")
-            for j in range(3):
-                GridMap.decode(GridMap.map[i][j])
-            print()
-    
-    # Check for row
-    def checkRow(row: int):
-        if len(set(GridMap.map[row])) == 1:
-            GridMap.hasWinner = True
-    
-    # Check for col
-    def checkCol(col: int):
-        grid_col = []
-        for i in range(3):
-            grid_col.append(GridMap.map[i][col])
-
-        if len(set(grid_col)) == 1:
-            GridMap.hasWinner = True
-
-    # Check for win
-    def checkWin(row: int, col: int):
-        if row != 1 and col != 1: # check corner
-            diag = []
-            if row == col:  # Check diagonal
-                for i in range(3):
-                    diag.append(GridMap.map[i][i])
-                if len(set(diag)) == 1:
-                    GridMap.hasWinner = True
-            else:
-                for i in range(3):
-                    diag.append(GridMap.map[2-i][i])
-                if len(set(diag)) == 1:
-                    GridMap.hasWinner = True
-        # All inputs must check row and col
-        GridMap.checkRow(row)
-        GridMap.checkCol(col)
-
-    
-    # Get the coords of input of the player
-    def getMapInput():
-        if GridMap.turn:
-            print("X turn")
-        else:
-            print("O turn")
-        row = int(input("Enter row: ")) - 1
-        col = int(input("Enter col: ")) - 1
-
-        # Check if valid to input
-        cell = GridMap.map[row][col]
-        if cell == '':
-            if GridMap.turn == True:
-                GridMap.map[row][col] = 'X'
-            else:
-                GridMap.map[row][col] = 'O'
-            GridMap.turn = not GridMap.turn
-            GridMap.numOfTurns += 1
-            GridMap.checkWin(row, col)
-        else:
-            print("\nCell occupied!\n")
-
-# Main Gameplay Loop
-def play():
-    GridMap.clear()
-    while not GridMap.hasWinner:    # Check for winner
-        GridMap.show()
-        if GridMap.numOfTurns >= 9: # Check if draw
-            print("Draw!")
-            return
-        GridMap.getMapInput()       # Get Input
-    GridMap.show()
-    if GridMap.turn:                # Evaluate Winner
-        print("O wins!")
-    else:
-        print("X wins!")
-
-# Shows game main menu
-def showMenu():
-    print("\n===== TIC-TAC-TOE =====")
-    print("[1] Play")
-    print("[2] Exit")
-    print("=======================")
-
-# Main Menu
-def main():
-    choice = None
-    while True:
-        showMenu()
-        choice = int(input("Choice: "))
-
-        # Evaluate choice
-        if choice == 1:
-            play()
-        elif choice == 2:
-            print("\nEXIT\n")
-            break
-        else:
-            print("\nINVALID INPUT\n")
+            # Horizontal
+            if (self.grid[i][0] == self.grid[i][1] == self.grid[i][2]) and self.grid[i][0] != '':
+                return self.grid[i][0]
+            # Vertical
+            if (self.grid[0][i] == self.grid[1][i] == self.grid[2][i]) and self.grid[0][i] != '':
+                return self.grid[0][i]
+        # Diagonal
+        if ((self.grid[0][0] == self.grid[1][1] == self.grid[2][2]) or (self.grid[0][2] == self.grid[1][1] == self.grid[2][0])) and self.grid[1][1] != '':
+            return self.grid[1][1]
         
+        return None
 
-# Run the app
+# static class that handles player turn
+class Player:
+    def get_turn(board: list) -> str:
+        num_x: int = 0    
+        num_o: int = 0
+        for row in board:
+            for val in row:
+                if val == 'X':
+                    num_x += 1
+                elif val == 'O':
+                    num_o += 1
+        if num_o >= num_x:
+            return 'X'
+        else:
+            return 'O'
+
+
+# Execute when file is ran
 if __name__ == '__main__':
-    main()
+
+    # game logic
+    my_board: Board = Board()
+
+    while(my_board.winner() == None):
+        my_board.show()
+        row: int = int(input('Row: '))
+        col: int = int(input('Col: '))
+        my_board.put(Player.get_turn(my_board.grid), (row-1, col-1))
+    
+    my_board.show()
+    print(f'{my_board.winner()} wins!')
